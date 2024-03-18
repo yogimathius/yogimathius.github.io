1b93ca32a86addd6ce9dc2663555242cb0453f1c
Mathius <info@yogimathius.dev>
2024-03-17 22:49:00 -0700

    feat: handle experience data dynamically

---
+++ scripts/experience.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const employmentContentContainer = document.querySelector('#employment-section');
+    fetch('./data/experience.json')
+        .then(response => response.json())
+        .then(employmentData => {
+            employmentData.employment.forEach(job => {
+                const article = document.createElement('article');
+                const h3 = document.createElement('h3');
+                const pPosition = document.createElement('p');
+                const pDuration = document.createElement('p');
+                const ulResponsibilities = document.createElement('ul');
+                
+                h3.textContent = job.company;
+                pPosition.innerHTML = `<strong>${job.position}</strong> · ${job.duration}`;
+                
+                job.responsibilities.forEach(responsibility => {
+                    const li = document.createElement('li');
+                    li.textContent = responsibility;
+                    ulResponsibilities.appendChild(li);
+                });
+                
+                article.appendChild(h3);
+                article.appendChild(pPosition);
+                article.appendChild(ulResponsibilities);
+                
+                employmentContentContainer.appendChild(article);
+            })
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        })
+});

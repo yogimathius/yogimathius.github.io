4c17c30c575848c3442d1895797711b068d488ac
Mathius <info@yogimathius.dev>
2024-03-17 22:33:29 -0700

    refactor: add about.js/about.json

---
+++ about.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const aboutContentContainer = document.querySelector('#about-me');
+    fetch('./about.json')
+        .then(response => response.json())
+        .then(aboutContent => {
+            aboutContent.mainContent.forEach(content => {
+                const contentDiv = document.createElement('article');
+                contentDiv.classList.add('card');
+                contentDiv.innerHTML = `<h2>${content.title}</h2><p>${content.body}</p>`;
+                aboutContentContainer.appendChild(contentDiv);
+            })
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        })
+});
--- index.js
+++ index.js
@@ -6,20 +6,4 @@ window.addEventListener('DOMContentLoaded', () => {
             link.classList.add('active');
         }
     });
-
-    const aboutContentContainer = document.querySelector('#about-me');
-    fetch('./index.json')
-    .then(response => response.json())
-    .then(aboutContent => {
-        console.log(aboutContent);
-        aboutContent.mainContent.forEach(content => {
-            const contentDiv = document.createElement('article');
-            contentDiv.classList.add('card');
-            contentDiv.innerHTML = `<h2>${content.title}</h2><p>${content.body}</p>`;
-            aboutContentContainer.appendChild(contentDiv);
-        })
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    })
 });
